969653c486fa36b07e12232f3356cf24ad2a444a
Tam Citrine <tam.citrine@gmail.com>
2025-04-12 22:09:59 -0700

    setup part2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-// https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
-
-import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind()]
 });
